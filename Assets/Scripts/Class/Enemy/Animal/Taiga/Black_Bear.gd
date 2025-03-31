da75a0d3578c5e29335cfce7946dc3d50b49eecf
Chasewp <167230071+Chasewp@users.noreply.github.com>
2025-03-31 08:59:28 +0700

    add_polar_bear_to the project

---
+++ Assets/Scripts/Class/Enemy/Animal/Taiga/Black_Bear.gd
@@ -0,0 +1,13 @@
+class_name Black_Bear
+extends Enemy_Main_Class
+
+
+func died():
+	queue_free()
+	MissionStatData.update_enemy_kills()
+
+func _process(delta):
+	move(1,speed)
+	move_and_slide()
+	update_state()
+	update_animation(delta)
